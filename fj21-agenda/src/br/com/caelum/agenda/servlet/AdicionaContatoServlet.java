71f0f69c72aea98c2581fab48476858c6d0cf195
web6554 <web6554@caelum114-01>
2017-03-18 14:48:26 -0300

    commit

---
+++ fj21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,58 @@
+package br.com.caelum.agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+
+@WebServlet("/adicionaContato")
+public class AdicionaContatoServlet extends HttpServlet {
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+		// super.service(arg0, arg1);
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+
+		} catch (java.text.ParseException e) {
+			// e.printStackTrace();
+			out.println("erro de conversao de data");
+			return;
+		}
+		// monta um objeto contato
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+		// salva contato
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+		// imprime o nome do contato que foi adicionado
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato" + contato.getNome() + " adicionado com sucesso");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
